556322d0579061d7ae96734fa60fe87527e8ccdc
Lokeshukla <30662507+Lokeshukla@users.noreply.github.com>
2017-08-28 22:35:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,10 +118,10 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });    
 
-app.get('article/:articleName', function (req, res) {
+app.get('articles/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
-  pool.query("SELECT * FROM WHERE title = " +req.parana.articleName, function (err,result) {
+  pool.query("SELECT * FROM WHERE title = '" +req.parana.articleName +"'", function (err,result) {
       if (err) {
         res.status(500).send(err.toString());
       }else {
